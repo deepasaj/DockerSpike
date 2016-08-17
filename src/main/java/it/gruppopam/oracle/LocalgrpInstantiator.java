00c983792b5b5f928e950baea33a2f73b7a35392
Sajani <deepasaj@thoughtworks.com>
2016-08-17 17:20:15 +0530

    Create uber jar

---
+++ src/main/java/it/gruppopam/oracle/LocalgrpInstantiator.java
@@ -0,0 +1,7 @@
+package it.gruppopam.oracle;
+
+public class LocalgrpInstantiator {
+    public static void main(String args[]) {
+        System.out.println("Inside LocalgrpInstantiator");
+    }
+}
